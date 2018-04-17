1d68037354e67dac181085407452aa18e6b579a4
Tomoyuki Ito <ito@nagisa-inc.jp>
2018-04-17 17:01:43 +0900

    build: add cocoapods

--- IGListKitSample/ViewController.swift
+++ IGListKitSample/ViewController.swift
@@ -13,6 +13,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let label = UILabel()
+        label.frame = CGRect(origin: .zero, size: CGSize(width: 200, height: 50))
+        label.center = view.center
+        label.textAlignment = .center
+        label.text = "hellow world"
+        view.addSubview(label)
     }
 
     override func didReceiveMemoryWarning() {
